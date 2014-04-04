b91d4fcca10a699d20f398ee7a727ae55f5543e4
Ulzee An <ul1994@gmail.com>
2014-04-03 20:12:24 -0500

    added client and server main files

---
+++ MP2/src/Client.java
@@ -0,0 +1,129 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import org.gstreamer.Caps;
+import org.gstreamer.Element;
+import org.gstreamer.ElementFactory;
+import org.gstreamer.Gst;
+import org.gstreamer.Pipeline;
+import org.gstreamer.State;
+import org.gstreamer.swing.VideoComponent;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Client {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		final ClientLauncher clientGUI = new ClientLauncher();
+		
+		clientGUI.buttons.get("Play").addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if (! clientGUI.connected) {
+					try {
+						Socket skt = new Socket("localhost", 45000);
+						skt.setReuseAddress(true);
+				        BufferedReader in = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+				        PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+				
+				        JSONObject response = new JSONObject(in.readLine());
+				        System.out.println("Successfully connected to server at 127.0.0.1:45000. Available files:");
+				        JSONArray files = response.getJSONArray("files");
+				        for(int i = 0; i < files.length(); i++)
+				        	System.out.println(i + ": " + files.getString(i));
+				        System.out.println("Which file would you like to play?");
+				        Scanner s = new Scanner(System.in);
+				        response = new JSONObject();
+				        response.put("request", files.getString(s.nextInt()));
+				        out.println(response.toString());
+	
+				        startStreaming();
+				        
+				        //send commands
+				        System.out.println("Listening for commands. Known commands include play, pause, and stop.");
+				        String line;
+				        while(true) {
+				        	line = s.nextLine();
+				        	response = new JSONObject();
+				        	response.put("command", line);
+				        	out.println(response.toString());
+				        	if(line.equals("stop"))
+				        		break;
+				        }
+				        
+				        in.close();
+				        out.close();
+				        skt.close();
+					} catch(Exception e) {
+						e.printStackTrace();
+					}
+					clientGUI.connected = true;
+				}
+			}
+		});
+	}
+
+	private static void startStreaming() {
+		Gst.init();
+		final Pipeline pipe = new Pipeline("pipeline");
+		final Element udpSrc = ElementFactory.make("udpsrc", "src");
+		// in the real thing these'll just get sent over the control stream, but for now they're hardcoded
+		udpSrc.setCaps(Caps.fromString("application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)JPEG, payload=(int)96, ssrc=(uint)2156703816, clock-base=(uint)1678649553, seqnum-base=(uint)31324" ));
+		udpSrc.set("uri", "udp://127.0.0.1:45001");
+		final Element depay = ElementFactory.make("rtpjpegdepay", "depay");
+		final Element decode = ElementFactory.make("jpegdec", "decode");
+		final Element color = ElementFactory.make("ffmpegcolorspace", "color");
+		//final Element sink = ElementFactory.make("autovideosink", "sink");
+		
+		// audio caps string is application/x-rtp, media=(string)audio, clock-rate=(int)44100, encoding-name=(string)L16, encoding-params=(string)2, channels=(int)2, payload=(int)96, ssrc=(uint)3489550614, clock-base=(uint)2613725642, seqnum-base=(uint)1704
+		Element udpAudSrc = ElementFactory.make("udpsrc", "src2");
+		udpAudSrc.setCaps(Caps.fromString("application/x-rtp, media=(string)audio, clock-rate=(int)44100, encoding-name=(string)L16, encoding-params=(string)2, channels=(int)2, payload=(int)96, ssrc=(uint)3489550614, clock-base=(uint)2613725642, seqnum-base=(uint)1704"));
+		udpAudSrc.set("uri", "udp://127.0.0.1:45002");
+		Element audDepay = ElementFactory.make("rtpL16depay", "auddepay");
+		Element audSink = ElementFactory.make("autoaudiosink", "audsink");
+		
+		pipe.addMany(udpAudSrc, audDepay, audSink);
+		Element.linkMany(udpAudSrc, audDepay, audSink);
+		
+		SwingUtilities.invokeLater(new Runnable() {
+
+            public void run() {
+            	/*
+                VideoComponent videoComponent = new VideoComponent();
+                Element videosink = videoComponent.getElement();
+                pipe.addMany(udpSrc, depay, decode, color, videosink);
+                Element.linkMany(udpSrc, depay, decode, color, videosink);
+                
+                // Now create a JFrame to display the video output
+                JFrame frame = new JFrame("Swing Video Test");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(videoComponent, BorderLayout.CENTER);
+                videoComponent.setPreferredSize(new Dimension(720, 576));
+                frame.pack();
+                frame.setVisible(true);
+                
+                // Start the pipeline processing
+                pipe.setState(State.PLAYING);
+                */
+            }
+        });
+	}
+
+}
---
+++ MP2/src/Server.java
@@ -0,0 +1,126 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import org.gstreamer.Bin;
+import org.gstreamer.Element;
+import org.gstreamer.ElementFactory;
+import org.gstreamer.GhostPad;
+import org.gstreamer.Gst;
+import org.gstreamer.State;
+import org.gstreamer.elements.PlayBin2;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ServerLauncher serverGUI = new ServerLauncher();
+		serverGUI.startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					ServerSocket srvr = new ServerSocket(45000);
+					srvr.setReuseAddress(true);
+			        Socket skt = srvr.accept();
+			        System.out.println("Client has connected from " + skt.getRemoteSocketAddress().toString());
+			        BufferedReader in = new BufferedReader(new InputStreamReader(skt.getInputStream()));
+			        PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
+			        // using JSON because it's easy
+			        JSONObject response = new JSONObject();
+			        
+			        File folder = new File(System.getProperty("user.dir") + "/videos/");
+			        for (final File fileEntry : folder.listFiles()) {
+			            if (fileEntry.isFile())
+			                response.append("files", fileEntry.getName());
+			        }
+			        out.println(response.toString());
+			        System.out.println("File list sent, awaiting response.");
+			        
+			        response = new JSONObject(in.readLine());
+			        String toStream = response.getString("request");
+			        System.out.println("Client requested " + toStream);
+			        
+			        PlayBin2 pb = startStreaming(toStream);
+			        
+			        // listen for commands
+			        while(pb.getState() != State.READY && pb.getState() != State.NULL) {
+			        	String nextCommand = in.readLine();
+			        	System.out.println("Received '" + nextCommand + "'.");
+			        	response = new JSONObject(nextCommand);
+			        	String command = response.getString("command");
+			        	switch(command) {
+			        	case "play":
+			        		pb.setState(State.PLAYING);
+			        		break;
+			        	case "pause":
+			        		pb.setState(State.PAUSED);
+			        		break;
+			        	case "stop":
+			        		pb.setState(State.NULL);
+			        		break;
+			        	}
+			        }
+			        
+			        in.close();
+			        out.close();
+			        skt.close();
+			        srvr.close();
+				} catch(Exception e2) {
+					e2.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	private static PlayBin2 startStreaming(String toStream) throws UnknownHostException, SocketException, InterruptedException {
+		final int port = 45001;
+		// create the pipeline here
+		Gst.init();
+		final PlayBin2 playbin = new PlayBin2("VideoPlayer");
+        playbin.setInputFile(new File("videos/" + toStream));
+        
+        Bin vidBin = new Bin("vidbin");
+        
+        Element encoder = ElementFactory.make("jpegenc", "encoder");
+		Element payloader = ElementFactory.make("rtpjpegpay", "payloader");
+		Element sink = ElementFactory.make("udpsink", "netsink");
+		sink.set("host", "127.0.0.1");
+		sink.set("port", "" + port);
+		sink.set("sync", "true");
+		vidBin.addMany(encoder, payloader, sink);
+		Element.linkMany(encoder, payloader, sink);
+		vidBin.addPad(new GhostPad("sink", encoder.getStaticPad("sink")));
+        
+        playbin.setVideoSink(vidBin);
+        
+        Bin audBin = new Bin("audbin");
+        
+        // assuming raw audio unless told otherwise
+        Element audConv = ElementFactory.make("audioconvert", "audioconv");
+        Element audPayload = ElementFactory.make("rtpL16pay", "audpay");
+        Element audSink = ElementFactory.make("udpsink", "aududpsink");
+        audSink.set("host", "127.0.0.1");
+        audSink.set("port", "" + (port + 1));
+        audSink.set("sync", "true");
+        audBin.addMany(audConv, audPayload, audSink);
+        Element.linkMany(audConv, audPayload, audSink);
+        audBin.addPad(new GhostPad("sink", audConv.getStaticPad("sink")));
+        
+        playbin.setAudioSink(audBin);
+        
+        playbin.setState(State.PLAYING);
+        //Gst.main();
+        //playbin.setState(State.NULL);
+        return playbin;
+	}
+
+}
